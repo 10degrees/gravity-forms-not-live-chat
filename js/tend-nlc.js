71b2dc156ad489fdaa0561f99f88930f811a8a35
Matt Radford <matt@10degrees.uk>
2017-01-20 13:24:09 +0000

    Comments and syntax issues

--- js/tend-nlc.js
+++ js/tend-nlc.js
@@ -1,11 +1,13 @@
 jQuery(document).ready(function ($) {
 
+  // Make it a clcik-to-call
   if ($(window).width() < 480) {
 
     $(".tend_nlc_chat").on('click', function () {
       $('a.tend_nlc_chat_content_call')[0].click();
     });
 
+  // Otherwise show the form
   } else {
 
     $('.tend_nlc_chat_icon').click(function () {
@@ -17,20 +19,22 @@ jQuery(document).ready(function ($) {
 
   }
 
+  // Hide modal on close icon click
   $('.tend_nlc_chat_close').click(function () {
     $('.tend_nlc_chat_content').slideToggle("slow", function () {
       $('.tend_nlc_chat_icon').show();
-    })
+    });
     $(".tend_nlc_chat").removeClass('open');
   });
 
+  // Hide the modal if there's a click on another part of the page
   $(document).mouseup(function (e) {
 		var container = $('.tend_nlc_chat');
 		if (!container.is(e.target) && container.has(e.target).length === 0) // nor a descendant of the container
 		{
 			$('.tend_nlc_chat_content').slideToggle("slow", function () {
         $('.tend_nlc_chat_icon').show();
-      })
+      });
       $(".tend_nlc_chat").removeClass('open');
 		}
 	});
