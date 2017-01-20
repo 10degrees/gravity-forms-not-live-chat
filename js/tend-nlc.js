a07b3332566c6cb13579075c2f47d5fa47bbf2df
Matt Radford <matt@10degrees.uk>
2017-01-20 13:22:49 +0000

    Add non-form click to hide

--- js/tend-nlc.js
+++ js/tend-nlc.js
@@ -9,13 +9,8 @@ jQuery(document).ready(function ($) {
   } else {
 
     $('.tend_nlc_chat_icon').click(function () {
-
       $(this).hide();
-
-      $('.tend_nlc_chat_content').slideToggle("slow", function () {
-
-      })
-
+      $('.tend_nlc_chat_content').slideToggle("slow");
       $(".tend_nlc_chat").addClass('open');
 
     });
@@ -28,5 +23,16 @@ jQuery(document).ready(function ($) {
     })
     $(".tend_nlc_chat").removeClass('open');
   });
+
+  $(document).mouseup(function (e) {
+		var container = $('.tend_nlc_chat');
+		if (!container.is(e.target) && container.has(e.target).length === 0) // nor a descendant of the container
+		{
+			$('.tend_nlc_chat_content').slideToggle("slow", function () {
+        $('.tend_nlc_chat_icon').show();
+      })
+      $(".tend_nlc_chat").removeClass('open');
+		}
+	});
 
 });
